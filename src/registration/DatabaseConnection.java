7129f28c8a046ed3e967b30398e4cf7f01d64087
Gaurav <gaurav556pant@gmail.com>
2023-03-09 00:56:53 +0530

    Verify Email ID and mobile number method in DatabaseConnection.java class

--- src/registration/DatabaseConnection.java
+++ src/registration/DatabaseConnection.java
@@ -3,7 +3,6 @@ package registration;
 import java.util.Scanner;
 
 public class DatabaseConnection {
-
 	public DatabaseConnection() {
 
 		/*
@@ -39,9 +38,10 @@ public class DatabaseConnection {
 		 * ********
 		 */
 		System.out.println("*************************");
-		System.out.println("Enter gmail id : ");
+		System.out.println("Enter Email id : ");
 		String gmail_Id = sc.nextLine();
-		rf.setGamilId(gmail_Id);
+		String emailId = email_Validation(gmail_Id);
+		rf.setGamilId(emailId);
 		/*
 		 * ********
 		 */
@@ -54,7 +54,7 @@ public class DatabaseConnection {
 		 * ********
 		 */
 		System.out.println("*************************");
-		System.out.println("Enter Mobile Number : ");
+		System.out.println("Enter User-Id : ");
 		String user_Id = sc.nextLine();
 		rf.setuserId(user_Id);
 		/*
@@ -70,31 +70,76 @@ public class DatabaseConnection {
 		System.out.println("*************************");
 
 	}
+
 	/*
-	 * There we are the Verify user mobile number 
+	 * 
+	 * There we are the Verify user Email Id
+	 * 
+	 */
+	private String email_Validation(String gmail_Id) {
+		int code1 = 0, code2 = 0;
+
+		for (int i = 0; i <= gmail_Id.length() - 1; i++) {
+			if (gmail_Id.contains(" ")) {
+				System.err.println("Wrong way to fill do not give any Space : ");
+				break;
+			} else {
+				if (gmail_Id.charAt(i) >= 48 && gmail_Id.charAt(i) <= 57) {
+					code1++;
+				}
+				if (gmail_Id.charAt(i) == 46 || gmail_Id.charAt(i) == 64) {
+					code2++;
+				}
+			}
+		}
+		/*
+		 * in there i am Facing a problem the problem is
+		 * 
+		 * if we update Wrong eamil-id it can't break it Continue running
+		 * 
+		 * we need to add the this code in loop then we break our code
+		 */
+
+		if (code1 > 0 && code2 > 0) {
+			System.out.println("Valid Email Id...");
+			return "valid Password";
+		} else {
+			System.err.println("InValid Email Id...");
+			return gmail_Id;
+
+		}
+
+	}
+
+	/*
+	 * There we are the Verify user mobile number
 	 */
 	private String number_Validation(String mobile_Number) {
 		int code1 = 0, code2 = 0;
-		if(mobile_Number.length()>=0&&mobile_Number.length()<=9) {
-			for(int i=0;i<=mobile_Number.length()-1;i++) {
-				if(mobile_Number.contains(" ")) {
+		if (mobile_Number.length() == 10) {
+			for (int i = 0; i <= mobile_Number.length() - 1; i++) {
+				if (mobile_Number.contains(" ")) {
 					System.err.println("Update Wright Mobile number...");
-				}else {
+					break;
+				} else {
 					if (mobile_Number.charAt(i) >= 0 && mobile_Number.charAt(i) <= 9) {
 						code1++;
+					} else {
+						break;
 					}
 				}
 			}
-		}else {
+		} else {
 			System.err.println("Update Wright Mobile number...");
+//			break;
 		}
-		
-		if(code1>0) {
+
+		if (code1 > 0) {
 			return "Valid Mobile Number...";
-		}else {
+		} else {
 			return "Invalid Mobile Number...";
 		}
-		
+
 	}
 
 	/*
@@ -107,23 +152,33 @@ public class DatabaseConnection {
 			for (int i = 0; i <= paw.length() - 1; i++) {
 				if (paw.contains(" ")) {
 					System.err.println("Wrong way to fill do not give any Space : ");
+					break;
 				} else {
 					if (paw.charAt(i) >= 'A' && paw.charAt(i) <= 'Z') {
 						code1++;
+					} else {
+						break;
 					}
 					if (paw.charAt(i) >= 'a' && paw.charAt(i) <= 'z') {
 						code2++;
+					} else {
+						break;
 					}
 					if (paw.charAt(i) >= 0 && paw.charAt(i) <= 9) {
 						code3++;
+					} else {
+						break;
 					}
 					if (paw.charAt(i) >= 35 && paw.charAt(i) <= 46) {
 						code4++;
+					} else {
+						break;
 					}
 				}
 			}
 		} else {
 			System.err.println("Invalid Password... \nPlease Fill again...");
+//			break;
 		}
 		if (code1 > 0 && code2 > 0 && code3 > 0 && code4 > 0) {
 			System.out.println("Valid Password...");
